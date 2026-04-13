dffd0983f7b1c4d0800c151705e8aa0f491ea9a2
Nemo2898 <lh2954361@gmail.com>
2026-04-13 10:05:20 +0300

    # added the export

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,27 @@
 export interface BaseType {
   id: number
   name: string
-  price: number
+  price?: number
   image_url: string
   barcode_url: string
 }
 
 export interface Bowl extends BaseType {
-  base_type_id: number
-  volume: number
+  base_type_id?: number
+  volume?: number
   slot_count: number
   shape: 'round' | 'square'
 }
+
+export interface Category {
+  id: number
+  name: string
+  base_type_id?: number
+}
+
+export interface Ingredient extends BaseType {
+  categoryId: number
+  diets: string[]
+  weight_grams?: number
+}
+
